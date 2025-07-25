91bcaafe1c6f6c4fb980207b3fd9b03374bf0ae8
khoa.nguyen <khoa.nguyen@fullertonhealth.com>
2025-07-25 11:57:13 +0700

    update

--- test/test_helpers.dart
+++ test/test_helpers.dart
@@ -1,15 +1,24 @@
 import 'package:selection_mode/selection_mode.dart';
+import 'package:selection_mode/src/core/selection_item_info.dart';
 
 extension TestHelpers on SelectionModeController {
   void registerTestItems(int count, {bool selectable = true}) {
     for (int i = 0; i < count; i++) {
-      registerItem(i, i, selectable);
+      register(SelectionItemInfo(
+        index: i,
+        identifier: i,
+        isSelectable: selectable,
+      ));
     }
   }
 
   void registerTestRange(int start, int end, {bool selectable = true}) {
     for (int i = start; i <= end; i++) {
-      registerItem(i, i, selectable);
+      register(SelectionItemInfo(
+        index: i,
+        identifier: i,
+        isSelectable: selectable,
+      ));
     }
   }
 }
